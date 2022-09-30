09fb38b97afb1a092e61358fb1aa4d47603ccc04
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-09-30 17:40:04 +0100

    Run through pre-commit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
   description = "Name of the region to deploy to"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "s3_prefix_list_id" {
   description = "Prefix list ID for S3 in the deployment region, as returned by `aws ec2 describe-prefix-lists`"
+  type        = string
   default     = "pl-63a5400a"
 }
