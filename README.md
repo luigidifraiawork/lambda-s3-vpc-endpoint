e266a906dc06fd947655871253e957c2eeb8fbc9
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-09-30 13:49:18 +0100

    A few rationalisations

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Based on a CloudFormation [project](https://github.com/gford1000-aws/lambda_s3_a
 
 Set up authentication details to your Sandbox (e.g. *A Cloud Guru*) with:
 ```commandline
-AWS_ACCESS_KEY_ID=AKIAZR3FF5EXAMPLEID
-AWS_SECRET_ACCESS_KEY=k9eabHZx0Kq0utFZ2u20Ymo1I7zaWEXAMPLEKEY
-AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=AKIAZR3FF5EXAMPLEID
+export AWS_SECRET_ACCESS_KEY=k9eabHZx0Kq0utFZ2u20Ymo1I7zaWEXAMPLEKEY
+export AWS_DEFAULT_REGION=us-east-1
 ```
 
 Ensure the above are correct by running:
--- main.tf
+++ main.tf
@@ -37,13 +37,6 @@ resource "random_pet" "this" {
   length = 2
 }
 
-/*
-resource "aws_kms_key" "objects" {
-  description             = "KMS key used to encrypt bucket objects"
-  deletion_window_in_days = 7
-}
-*/
-
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/3.16.0
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -57,88 +50,33 @@ module "vpc" {
 }
 
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/3.16.0/submodules/vpc-endpoints
-#module "vpc_endpoints" {
-#  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-#  version = "3.16.0"
-#
-#  vpc_id          = module.vpc.vpc_id
-#
-#  endpoints = {
-#    s3 = {
-#      service         = "s3"
-#      service_type    = "Gateway"
-#      route_table_ids = module.vpc.private_route_table_ids
-#      policy          = jsonencode(
-#        {
-#            Version = "2012-10-17"
-#            Statement = [
-#    /*
-#                {
-#                    Effect = "Allow"
-#                    Principal = "*"
-#                    Action = [
-#                        "s3:ListBucket",
-#                    ]
-#                    Resource = [
-#                        module.s3_bucket.s3_bucket_arn,
-#                    ]
-#                },
-#    */
-#                {
-#                    Effect = "Allow"
-#                    Principal = "*"
-#                    Action = [
-#                        //"s3:GetObject",
-#                        "s3:PutObject",
-#                    ]
-#                    Resource = [
-#                        "arn:aws:s3:::${module.s3_bucket.s3_bucket_id}/*",
-#                    ]
-#                }
-#            ]
-#        }
-#      )
-#
-#    }
-#  }
-#}
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "3.16.0"
 
-# https://registry.terraform.io/providers/hashicorp/aws/4.33.0/docs/resources/vpc_endpoint
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id          = module.vpc.vpc_id
-  service_name    = "com.amazonaws.${var.region}.s3"
-  route_table_ids = module.vpc.private_route_table_ids # Try module.vpc.vpc_main_route_table_id too
+  vpc_id = module.vpc.vpc_id
 
-  policy = jsonencode(
-    {
-        Version = "2012-10-17"
-        Statement = [
-/*
-            {
-                Effect = "Allow"
-                Principal = "*"
-                Action = [
-                    "s3:ListBucket",
-                ]
-                Resource = [
-                    module.s3_bucket.s3_bucket_arn,
-                ]
-            },
-*/
-            {
-                Effect = "Allow"
-                Principal = "*"
-                Action = [
-                    //"s3:GetObject",
-                    "s3:PutObject",
-                ]
-                Resource = [
-                    "arn:aws:s3:::${module.s3_bucket.s3_bucket_id}/*",
-                ]
-            }
-        ]
+  endpoints = {
+    s3 = {
+      service         = "s3"
+      service_type    = "Gateway"
+      route_table_ids = module.vpc.private_route_table_ids
+      policy          = data.aws_iam_policy_document.endpoint.json
     }
-  )
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/4.33.0/docs/data-sources/iam_policy_document
+data "aws_iam_policy_document" "endpoint" {
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${module.s3_bucket.s3_bucket_id}/*",
+    ]
+  }
 }
 
 # https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/3.4.0
@@ -153,17 +91,6 @@ module "s3_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
-/*
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-*/
 }
 
 # https://registry.terraform.io/modules/terraform-aws-modules/lambda/aws/4.0.2
@@ -182,14 +109,14 @@ module "lambda_s3_write" {
   source_path = "${path.module}/fixtures/python3.8-app"
 
   environment_variables = {
-    BucketName = module.s3_bucket.s3_bucket_id
-    RegionName = var.region
+    BUCKET_NAME = module.s3_bucket.s3_bucket_id
+    REGION_NAME = var.region
   }
 
   vpc_security_group_ids = [module.security_group_lambda.security_group_id]
   vpc_subnet_ids         = module.vpc.private_subnets
 
-  attach_network_policy  = true
+  attach_network_policy = true
 }
 
 # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/4.13.1
@@ -230,11 +157,11 @@ module "iam_policy_lambda" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "5.5.0"
 
-  name = random_pet.this.id
+  name   = random_pet.this.id
   policy = data.aws_iam_policy_document.lambda.json
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+# https://registry.terraform.io/providers/hashicorp/aws/4.33.0/docs/data-sources/iam_policy_document
 data "aws_iam_policy_document" "lambda" {
   statement {
     actions = [
@@ -247,20 +174,8 @@ data "aws_iam_policy_document" "lambda" {
       "arn:aws:logs:*:*:*",
     ]
   }
-/*
   statement {
     actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      module.s3_bucket.s3_bucket_arn,
-    ]
-  }
-*/
-  statement {
-    actions = [
-      //"s3:GetObject",
       "s3:PutObject",
     ]
 
