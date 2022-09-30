124a6466b2a03a9a2f1c43c824e3d7d6b477f0f0
luigidifraiawork <93160889+luigidifraiawork@users.noreply.github.com>
2022-09-30 15:24:22 +0100

    Remove unused config

--- fixtures/python3.8-app/index.py
+++ fixtures/python3.8-app/index.py
@@ -1,5 +1,4 @@
 import boto3
-import botocore.config
 import os
 from uuid import uuid4
 
@@ -7,9 +6,7 @@ bucketName = os.environ['BUCKET_NAME']
 regionName = os.environ['REGION_NAME']
 
 def lambda_handler(event, context):
-    # Using S3 VPC Endpoint requires 'path' style addressing, to avoid global url resolution
-    # Create client per: http://boto3.readthedocs.io/en/latest/guide/s3.html
-    client = boto3.client('s3', regionName, config=botocore.config.Config(s3={'addressing_style': 'path'}))
+    client = boto3.client('s3', regionName)
     resp = client.put_object(
        Bucket=bucketName,
        Key=str(uuid4()),
