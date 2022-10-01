43fcf46964056957979e022e9fa8a94dc4488779
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-10-01 08:27:56 +0100

    Automatically retrieve the prefix list ID for the s3 endpoint

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ module "security_group_lambda" {
   vpc_id = module.vpc.vpc_id
 
   # Prefix list ids to use in all egress rules in this module
-  egress_prefix_list_ids = [var.s3_prefix_list_id]
+  egress_prefix_list_ids = [module.vpc_endpoints.endpoints["s3"]["prefix_list_id"]]
 
   egress_rules = ["all-all"]
 }
--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "s3_prefix_list_id" {
-  description = "Prefix list ID for S3 in the deployment region, as returned by `aws ec2 describe-prefix-lists`"
-  type        = string
-  default     = "pl-63a5400a"
-}
