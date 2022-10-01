5b6bb84b07d93a830dad3010acce93b5ffdd98d7
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-10-01 09:11:25 +0100

    Consistently use bucket ARN for resources in policies

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ data "aws_iam_policy_document" "endpoint" {
     ]
 
     resources = [
-      "arn:aws:s3:::${module.s3_bucket.s3_bucket_id}/*",
+      "${module.s3_bucket.s3_bucket_arn}/*",
     ]
   }
 }
