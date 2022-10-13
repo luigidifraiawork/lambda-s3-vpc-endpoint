5423d933a72ccfa8fad58b8e5250949aff032ff0
Luigi Di Fraia <luigidifraia+workrelated@gmail.com>
2022-10-13 19:25:33 +0100

    Rationalise vars

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "vpc_cidr" {
-  description = "CIDR Block to allocate to the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-  validation {
-    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}(\\/([0-9]|[1-2][0-9]|3[0-2]))?$", var.vpc_cidr))
-    error_message = "The value of variable 'vpc_cidr' must be a valid network CIDR: a.b.c.d/m."
-  }
-}
-
 variable "region" {
   description = "Name of the region to deploy to"
   type        = string
@@ -18,13 +8,23 @@ variable "region" {
   }
 }
 
+variable "vpc_cidr" {
+  description = "CIDR Block to allocate to the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}(\\/([0-9]|[1-2][0-9]|3[0-2]))?$", var.vpc_cidr))
+    error_message = "The value of variable 'vpc_cidr' must be a valid network CIDR: a.b.c.d/m."
+  }
+}
+
 variable "az_count" {
   description = "Number of availability zones to create VPC subnets in"
   type        = number
   default     = 3
   validation {
-    condition     = var.az_count >= 3 && var.az_count <= 6
-    error_message = "The value of variable 'az_count' must be between 2 and 6."
+    condition     = var.az_count >= 1 && var.az_count <= 6
+    error_message = "The value of variable 'az_count' must be between 1 and 6."
   }
 }
 
